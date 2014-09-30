8760a6def34edb772c31ea5c0d313e4a9b54cc1a
dav-col <collinsd@unm.edu>
2014-09-30 09:43:33 -0600

    simple OpenCL fitness function

--- Genetic Algorithm/kernels/simpleFitness.cl
+++ Genetic Algorithm/kernels/simpleFitness.cl
@@ -4,14 +4,14 @@ kernel void simpleFitness(global const int* a, global const int* b, global int* 
  
   if (iGID < numElements)
   {
-  int aColor = a[iGID];
-  int bColor = b[iGID];
+    int aColor = a[iGID];
+    int bColor = b[iGID];
   
-  int dRed = (aColor >> 16) & 0xFF - (bColor >> 16) & 0xFF;
-  int dGreen = (aColor >> 8) & 0xFF - (bColor >> 8) & 0xFF;
-  int dBlue = (aColor & 0xFF) - (bColor & 0xFF);
+    int dRed = (aColor >> 16) & 0xFF - (bColor >> 16) & 0xFF;
+    int dGreen = (aColor >> 8) & 0xFF - (bColor >> 8) & 0xFF;
+    int dBlue = (aColor & 0xFF) - (bColor & 0xFF);
   
   
-  c[iGID] = dRed + dGreen + dBlue;
+    c[iGID] = dRed + dGreen + dBlue;
   }
 }
--- Genetic Algorithm/src/ImageEvolver/FitnessFunctions.java
+++ Genetic Algorithm/src/ImageEvolver/FitnessFunctions.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 import ImageEvolver.util.Constants;
@@ -22,96 +23,100 @@ import com.jogamp.opencl.CLProgram;
 
 public class FitnessFunctions
 {
+  private static CLProgram program;
+  private static CLKernel kernel;
   public static int getSimpleFitness(BufferedImage source, BufferedImage target)
   {
-//    int fitness = 0;
-//    int numElements = 0;
-//
-//    CLContext context = CLContext.create();
-//
-//    try
-//    {
-//      CLDevice device = context.getMaxFlopsDevice();
-//      CLCommandQueue queue = device.createCommandQueue();
-//
-//      numElements = Constants.width * Constants.height;
-//
-//      int localWorkSize = Math.min(256, device.getMaxWorkGroupSize());
-//      int globalWorkSize = (int) pow(2.0, ceil(log(numElements) / log(2.0)));
-//
-//      File sourceFile = new File("kernels/simpleFitness.cl");
-//      FileInputStream sourceInputStream = new FileInputStream(sourceFile);
-//      CLProgram program = context.createProgram(sourceInputStream).build();
-//
-//      /*
-//      CLBuffer<IntBuffer> clBufferA = context.createIntBuffer(globalWorkSize,
-//          CLMemory.Mem.READ_ONLY);
-//      CLBuffer<IntBuffer> clBufferB = context.createIntBuffer(globalWorkSize,
-//          CLMemory.Mem.READ_ONLY);*/
-//      CLBuffer<IntBuffer> clBufferC = context.createIntBuffer(globalWorkSize,
-//          CLMemory.Mem.WRITE_ONLY);
-//
-//      CLBuffer<IntBuffer> clBufferA = context.createBuffer(fillBuffer(source), CLMemory.Mem.READ_ONLY);
-//      CLBuffer<IntBuffer> clBufferB = context.createBuffer(fillBuffer(target), CLMemory.Mem.READ_ONLY);
-//
-//      CLKernel kernel = program.createCLKernel("simpleFitness");
-//      kernel.putArgs(clBufferA, clBufferB, clBufferC).putArg(numElements);
-//
-//      queue.putWriteBuffer(clBufferA, true);
-//      queue.putWriteBuffer(clBufferB, true);
-//      queue.put1DRangeKernel(kernel, 0, globalWorkSize, localWorkSize);
-//      queue.putReadBuffer(clBufferC, true);
-//
-//      int[] result = new int[numElements];
-//      clBufferC.getBuffer().get(result);
-//      for (int i = 0; i < numElements; i++)
-//        fitness += result[i];
-//    }
-//    catch (IOException e)
-//    {
-//      e.printStackTrace();
-//    }
-//    finally
-//    {
-//      context.release();
-//    }
+    int fitness = 0;
+    int numElements = 0;
 
-     int sourceWidth = source.getWidth();
-     int sourceHeight = source.getHeight();
-     int targetWidth = target.getWidth();
-     int targetHeight = target.getHeight();
-    
-     int fitness = 0;
-    
-     if (sourceWidth != targetWidth || sourceHeight != targetHeight)
-     {
-     System.err.println("Fitness error: dimension mismatch");
-     return 0;
-     }
-    
-     for (int i = 0; i < sourceWidth; i++)
-     {
-     for (int j = 0; j < sourceHeight; j++)
-     {
-     int sourceColor = source.getRGB(i, j);
-     int targetColor = target.getRGB(i, j);
-    
-     int dRed = Math.abs((sourceColor >> 16) & 0xFF - (targetColor >> 16) & 0xFF);
-     int dGreen = Math.abs((sourceColor >> 8) & 0xFF - (targetColor >> 8) & 0xFF);
-     int dBlue = Math.abs(sourceColor & 0xFF - targetColor & 0xFF);
-    
-     fitness += dRed + dGreen + dBlue;
-     }
-     }
-     fitness = fitness / (sourceWidth * sourceHeight);
-    
+    CLContext context = CLContext.create();
+
+    try
+    {
+      CLDevice device = context.getMaxFlopsDevice();
+      CLCommandQueue queue = device.createCommandQueue();
+
+      numElements = Constants.width * Constants.height;
+
+      int localWorkSize = Math.min(256, device.getMaxWorkGroupSize());
+      int globalWorkSize = (int) pow(2.0, ceil(log(numElements) / log(2.0)));
+
+      CLBuffer<IntBuffer> clBufferC = context.createIntBuffer(globalWorkSize, CLMemory.Mem.WRITE_ONLY);
+
+      CLBuffer<IntBuffer> clBufferA = context.createBuffer(fillBuffer(source), CLMemory.Mem.READ_ONLY);
+      CLBuffer<IntBuffer> clBufferB = context.createBuffer(fillBuffer(target), CLMemory.Mem.READ_ONLY);
+      
+
+      File sourceFile = new File("kernels/simpleFitness.cl");
+      FileInputStream sourceInputStream = new FileInputStream(sourceFile);
+      program = context.createProgram(sourceInputStream).build();
+      kernel = program.createCLKernel("simpleFitness");
+      kernel.putArgs(clBufferA, clBufferB, clBufferC).putArg(numElements);
+      
+      queue.putWriteBuffer(clBufferA, true)
+           .putWriteBuffer(clBufferB, true)
+           .put1DRangeKernel(kernel, 0, globalWorkSize, localWorkSize)
+           .putReadBuffer(clBufferC, true);
+
+      kernel.release();
+      
+      
+      int[] result = new int[numElements];
+      clBufferC.getBuffer().get(result);
+      for (int i = 0; i < numElements; i++) fitness += result[i];
+      
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      context.release();
+    }
+    fitness = fitness / (source.getWidth() * source.getHeight());
     return fitness;
+
+//    int sourceWidth = source.getWidth();
+//    int sourceHeight = source.getHeight();
+//    int targetWidth = target.getWidth();
+//    int targetHeight = target.getHeight();
+//
+//    int fitness = 0;
+//
+//    if (sourceWidth != targetWidth || sourceHeight != targetHeight)
+//    {
+//      System.err.println("Fitness error: dimension mismatch");
+//      return 0;
+//    }
+//
+//    for (int i = 0; i < sourceWidth; i++)
+//    {
+//      for (int j = 0; j < sourceHeight; j++)
+//      {
+//        int sourceColor = source.getRGB(i, j);
+//        int targetColor = target.getRGB(i, j);
+//
+//        int dRed = Math.abs((sourceColor >> 16) & 0xFF - (targetColor >> 16) & 0xFF);
+//        int dGreen = Math.abs((sourceColor >> 8) & 0xFF - (targetColor >> 8) & 0xFF);
+//        int dBlue = Math.abs(sourceColor & 0xFF - targetColor & 0xFF);
+//
+//        fitness += dRed + dGreen + dBlue;
+//      }
+//    }
+//    fitness = fitness / (sourceWidth * sourceHeight);
+//
+//    return fitness;
   }
-//
-//  private static IntBuffer fillBuffer(BufferedImage image)
-//  {
-//    
-//    return IntBuffer.wrap(rgb);
-//
-//  }
+
+  private static IntBuffer fillBuffer(BufferedImage image)
+  {
+    IntBuffer buffer = ByteBuffer.allocateDirect(image.getWidth() * image.getHeight() * 4).asIntBuffer(); 
+    buffer.put(image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth()));
+    buffer.rewind();
+    
+    return buffer;
+
+  }
 }
