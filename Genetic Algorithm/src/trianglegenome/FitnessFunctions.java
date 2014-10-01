a6c5f41d172dfb470f5af425d27f3f4b694243a2
dav-col <collinsd@unm.edu>
2014-09-30 21:02:54 -0600

    Formatting fix

--- Genetic Algorithm/src/trianglegenome/FitnessFunctions.java
+++ Genetic Algorithm/src/trianglegenome/FitnessFunctions.java
@@ -58,35 +58,35 @@ public class FitnessFunctions
 //      context.release();
 //    }
 
-     int sourceWidth = source.getWidth();
-     int sourceHeight = source.getHeight();
-     int targetWidth = target.getWidth();
-     int targetHeight = target.getHeight();
-    
-     int fitness = 0;
-    
-     if (sourceWidth != targetWidth || sourceHeight != targetHeight)
-     {
-     System.err.println("Fitness error: dimension mismatch");
-     return 0;
-     }
-    
-     for (int i = 0; i < sourceWidth; i++)
-     {
-     for (int j = 0; j < sourceHeight; j++)
-     {
-     int sourceColor = source.getRGB(i, j);
-     int targetColor = target.getRGB(i, j);
-    
-     int dRed = Math.abs((sourceColor >> 16) & 0xFF - (targetColor >> 16) & 0xFF);
-     int dGreen = Math.abs((sourceColor >> 8) & 0xFF - (targetColor >> 8) & 0xFF);
-     int dBlue = Math.abs(sourceColor & 0xFF - targetColor & 0xFF);
-    
-     fitness += dRed + dGreen + dBlue;
-     }
-     }
-     fitness = fitness / (sourceWidth * sourceHeight);
-    
+    int sourceWidth = source.getWidth();
+    int sourceHeight = source.getHeight();
+    int targetWidth = target.getWidth();
+    int targetHeight = target.getHeight();
+
+    int fitness = 0;
+
+    if (sourceWidth != targetWidth || sourceHeight != targetHeight)
+    {
+      System.err.println("Fitness error: dimension mismatch");
+      return 0;
+    }
+
+    for (int i = 0; i < sourceWidth; i++)
+    {
+      for (int j = 0; j < sourceHeight; j++)
+      {
+        int sourceColor = source.getRGB(i, j);
+        int targetColor = target.getRGB(i, j);
+
+        int dRed = Math.abs((sourceColor >> 16) & 0xFF - (targetColor >> 16) & 0xFF);
+        int dGreen = Math.abs((sourceColor >> 8) & 0xFF - (targetColor >> 8) & 0xFF);
+        int dBlue = Math.abs(sourceColor & 0xFF - targetColor & 0xFF);
+
+        fitness += dRed + dGreen + dBlue;
+      }
+    }
+    fitness = fitness / (sourceWidth * sourceHeight);
+
     return fitness;
   }
 //
