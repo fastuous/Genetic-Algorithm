dd537b5091390a76e1b40817a082fb05023ffb69
dav-col <collinsd@unm.edu>
2014-10-01 10:00:12 -0600

    Updated crossover

--- Genetic Algorithm/src/trianglegenome/Genome.java
+++ Genetic Algorithm/src/trianglegenome/Genome.java
@@ -29,30 +29,69 @@ public class Genome implements Cloneable
   }
   
   /**
+   * Given two parents and two children, performs a double point crossover
+   * of the two parents and writes the data to the two children. 
+   * @param parent1 The first parent of the crossover.
+   * @param parent2 The second parent of the crossover.
+   * @param child1 The first child, to which the data will be overwritten.
+   * @param child2 The second child, to which the data will be overwritten.
+   * @param start The starting point of the swap (inclusive).
+   * @param end The ending point of the swap (exclusive).
+   */
+  public static void doublePointCrossOver(
+      Genome parent1, Genome parent2,
+      Genome child1, Genome child2,
+      int start, int end)
+  {
+    child1.copyFrom(parent1);
+    child2.copyFrom(parent2);
+    doublePointCrossoverInPlace(child1, child2, start, end);
+  }
+  
+  /**
+   * Given two parents and two children, performs a single point crossover
+   * of the two parents and writes the data to the two children. 
+   * @param parent1 The first parent of the crossover.
+   * @param parent2 The second parent of the crossover.
+   * @param child1 The first child, to which the data will be overwritten.
+   * @param child2 The second child, to which the data will be overwritten.
+   * @param end The ending point of the swap (exclusive).
+   */
+  public static void singlePointCrossOver(
+      Genome parent1, Genome parent2,
+      Genome child1, Genome child2,
+      int end)
+  {
+    child1.copyFrom(parent1);
+    child2.copyFrom(parent2);
+    singlePointCrossoverInPlace(child1, child2, end);
+  }
+
+  /**
    * Given two triangle Genomes, performs a crossover at a given start point and end point.
    * The ending point should not be greater than the number of triangles in a Genome
    * multiplied by the triangle DNA length.
    * @param a The first Genome of the crossover.
    * @param b The second Genome of the crossover.
-   * @param startInclusive The starting point of the swap.
-   * @param endExclusive The ending point of the swap.
+   * @param start The starting point of the swap (inclusive).
+   * @param end The ending point of the swap (exclusive).
    */
-  public static void doublePointCrossover(Genome a, Genome b, int startInclusive, int endExclusive)
+  public static void doublePointCrossoverInPlace(Genome a, Genome b, int start, int end)
   {
     // The swap ending point for the last triangle pair to be swapped.
-    int end2 = endExclusive % Triangle.DNA_LENGTH;
+    int end2 = end % Triangle.DNA_LENGTH;
 
     // The swap starting point for the first triangle pair to be swapped.
-    int start1 = startInclusive % Triangle.DNA_LENGTH;
+    int start1 = start % Triangle.DNA_LENGTH;
     
     // Number of triangles that can be fully swapped.
-    int fullSwaps = ((endExclusive - end2) - (startInclusive + start1)) / Triangle.DNA_LENGTH;
+    int fullSwaps = ((end - end2) - (start + start1)) / Triangle.DNA_LENGTH;
     
     // The first triangle that will be swapped.
-    int firstTriangle = startInclusive / Triangle.DNA_LENGTH;
+    int firstTriangle = start / Triangle.DNA_LENGTH;
     
     // The last triangle to be swapped.
-    int lastTriangle = endExclusive / Triangle.DNA_LENGTH;
+    int lastTriangle = end / Triangle.DNA_LENGTH;
     
     List<Triangle> aSubList = a.genes.subList(firstTriangle, lastTriangle + 1);
     List<Triangle> bSubList = b.genes.subList(firstTriangle, lastTriangle + 1);
@@ -74,6 +113,30 @@ public class Genome implements Cloneable
   }
   
   /**
+   * Given two triangle Genomes, performs a crossover from the beginning to a given end point.
+   * The ending point should not be greater than the number of triangles in a Genome
+   * multiplied by the triangle DNA length.
+   * @param a The first Genome of the crossover.
+   * @param b The second Genome of the crossover.
+   * @param end The ending point of the swap (exclusive).
+   */
+  public static void singlePointCrossoverInPlace(Genome a, Genome b, int end)
+  {
+    doublePointCrossoverInPlace(a, b, 0, end);
+  }
+  
+  /**
+   * Clears this Genome's {{@link #genes} and clones each {@link trianglegenome.Triangle}
+   * from a different Genome into this Genome's genes. 
+   * @param other The other Genome from which to copy the genes.
+   */
+  private void copyFrom(Genome other)
+  {
+    this.genes.clear();
+    other.genes.forEach(t -> this.addGene(t.clone()));
+  }
+  
+  /**
    * Returns a deep-copy of this Genome.
    * @return A deep-copy of this Genome.
    */
@@ -83,18 +146,5 @@ public class Genome implements Cloneable
     Genome copy = new Genome();
     this.genes.forEach(t -> copy.addGene(t.clone()));
     return copy;
-  }
-  
-  /**
-   * Given two triangle Genomes, performs a crossover from the beginning to a given end point.
-   * The ending point should not be greater than the number of triangles in a Genome
-   * multiplied by the triangle DNA length.
-   * @param a The first Genome of the crossover.
-   * @param b The second Genome of the crossover.
-   * @param endExclusive The ending point of the swap.
-   */
-  public static void singlePointCrossover(Genome a, Genome b, int endExclusive)
-  {
-    doublePointCrossover(a, b, 0, endExclusive);
   }
 }
