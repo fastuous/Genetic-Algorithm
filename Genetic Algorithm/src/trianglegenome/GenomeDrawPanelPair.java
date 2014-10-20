8a67d7638e4975e3636ba9b58f560fc0252360b4
dav-col <collinsd@unm.edu>
2014-10-20 14:37:15 -0600

    Moved GenomeState to GenomeDrawPanelPair

--- Genetic Algorithm/src/trianglegenome/GenomeDrawPanelPair.java
+++ Genetic Algorithm/src/trianglegenome/GenomeDrawPanelPair.java
@@ -4,31 +4,19 @@ import trianglegenome.gui.DrawPanel;
 
 /**
  * A class to keep track of a mapping between a {@link trianglegenome.Genome} and a
- * {@link trianglegenome.gui.DrawPanel}. This class also has another Genome member to
- * keep track of a previous state.
+ * {@link trianglegenome.gui.DrawPanel}.
  * <br /><br />
  * Example code:<br/>
  * <code><pre>
  *  Genome g = RandomGenome.generateGenome();
- *  DrawPanel d = new DrawPanel(640, 480);
- *  d.setTriangles(g.getGenes());
- *  d.repaint();
+ *  DrawPanel d = new DrawPanel(640, 480); 
  *  
- *  GenomeState s = new GenomeState(g, d);
- *  
- *  // Mutate the Genome g
- *  // p.genome is now mutated but p.previous is preserved.
- *  
- *  s.drawPanel.setTriangles(g.getGenes());
- *  s.drawPanel.repaint();
- *  
- *  // Check if the mutation was good and if it was do the following:
- *  s.previous.copyFrom(p.genome);
+ *  GenomeDrawPanelPair gdpp = new GenomeDrawPanelPair(g, d);
  *  
  * </pre></code>
  * @author collinsd
  */
-public class GenomeState
+public class GenomeDrawPanelPair
 {
   /** A Genome meant to keep track of the previous state.
    * Use {@link trianglegenome.Genome#clone() to set this initially. */
@@ -40,12 +28,12 @@ public class GenomeState
   public DrawPanel drawPanel;
   
   /**
-   * Creates a new GenomePanelPair where {@link GenomeState#previous}
-   * is a deep copy of {@link GenomeState#genome}.
+   * Creates a new GenomePanelPair where {@link GenomeDrawPanelPair#previous}
+   * is a deep copy of {@link GenomeDrawPanelPair#genome}.
    * @param genome The Genome in the Genome/DrawPanel mapping.
    * @param drawPanel The DrawPanel in the Genome/DrawPanel mapping. 
    */
-  public GenomeState(Genome genome, DrawPanel drawPanel)
+  public GenomeDrawPanelPair(Genome genome, DrawPanel drawPanel)
   {
     this.genome = genome;
     this.drawPanel = drawPanel;
