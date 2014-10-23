280a467d7f42f4276f2af421e41d96aa618c6da8
dav-col <collinsd@unm.edu>
2014-10-22 21:47:31 -0600

    Quick format fix

--- Genetic Algorithm/src/trianglegenome/HillClimbing.java
+++ Genetic Algorithm/src/trianglegenome/HillClimbing.java
@@ -161,8 +161,7 @@ public class HillClimbing extends Thread
         stepSize = Constants.rand.nextInt(2)+1;
         triangle = Constants.rand.nextInt(Constants.TRIANGLE_COUNT);
       }
-    }
-    while (fitnessAfter > fitnessBefore);
+    } while (fitnessAfter > fitnessBefore);
     successfulMultiplier += .5;
   }
 }
