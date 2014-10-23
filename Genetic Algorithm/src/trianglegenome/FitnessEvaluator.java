a61def27c41b6e8549ec65f916a2be2381180c6f
dav-col <collinsd@unm.edu>
2014-10-23 08:50:43 -0600

    Removed semaphore from FitnessEvaluator

--- Genetic Algorithm/src/trianglegenome/FitnessEvaluator.java
+++ Genetic Algorithm/src/trianglegenome/FitnessEvaluator.java
@@ -76,8 +76,6 @@ public class FitnessEvaluator
   private final String ERROR_SIZE = "Reference and triangle images must be the same size"; 
   private final String ERROR_TYPE = "Image must be of type BufferedImage.TYPE_INT_RGB"; 
   
-  private static Semaphore semaphore = new Semaphore(Constants.MAX_CONCURRENT_OPENCL_EVALS);
-  
   /**
    * Creates a new fitness evaluator.
    */
@@ -149,9 +147,6 @@ public class FitnessEvaluator
    */
   public int differenceSumCL(BufferedImage triangles)
   {
-    try { semaphore.acquire(); }
-    catch (Exception e) {}
-    
     CLBuffer<IntBuffer> trianglesCLBuffer;
     DataBufferInt trtianglesBufferInt;
 
@@ -175,8 +170,6 @@ public class FitnessEvaluator
     for (int i = 0; i < elementCount; i++) sum += differences.get();
     
     trianglesCLBuffer.release();
-
-    semaphore.release();
     
     return sum;
   }
